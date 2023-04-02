79a50c25ca366740fe01e8d36a232ab6cd1c9b4f
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2023-04-02 12:22:45 -0400

    threads in loop

--- main.c
+++ main.c
@@ -3,29 +3,37 @@
 #include <pthread.h>
 
 int mails = 0;
+pthread_mutex_t mutex;
 
 void* routine() {
-    for (int i = 0; i < 1000000; i++) {
+    for (int i = 0; i < 5000000; i++) {
+        pthread_mutex_lock(&mutex);
         mails++;
+        pthread_mutex_unlock(&mutex);
     }
 }
 
 int main(int argc, char* argv[]) {
-    pthread_t p1, p2;
+    pthread_t th[4];
+    int i;
 
-    if (pthread_create(&p1, NULL, &routine, NULL) != 0) {
-        return 1;
-    }
-    if (pthread_create(&p2, NULL, &routine, NULL) != 0) {
-        return 2;
-    }
-    if (pthread_join(p1, NULL) != 0) {
-        return 3;
-    }
-    if (pthread_join(p2, NULL) != 0) {
-        return 4;
+    pthread_mutex_init(&mutex, NULL);
+
+    for (int i = 0; i < 4; i++) {
+        if (pthread_create(&th[i], NULL, &routine, NULL) != 0) {
+            return 1;
+        }
+        printf("Thread %d has started.\n", i);
     }
 
+    for (int i = 0; i < 4; i++) {
+        if (pthread_join(th[i], NULL) != 0) {
+            return 2;
+        }
+        printf("Thread %d has finished.\n", i);
+    }
+
+    pthread_mutex_destroy(&mutex);
     printf("Number of mails: %d\n", mails);
 
     return 0;
